2de682dbdbd73881f5eda80be7cb7976ba002f49
Muhammad Kaisar Teddy <145271130+muhkai@users.noreply.github.com>
2024-04-24 17:27:44 +0700

    122140058_Muhammad Kaisar Teddy_Pset{1}.py

--- week5-pbo-rc/122140058_Muhammad Kaisar Teddy_Pset{1}.py
+++ week5-pbo-rc/122140058_Muhammad Kaisar Teddy_Pset{1}.py
@@ -28,8 +28,7 @@ class Login:
         tk.Button(self.boxlogin, text='Register', command=self.open_register).pack()
 
         self.users = {'kaisar': 'yangtautauaja', 'kopraljono': 'jenderal'}
-
-        # Event binding untuk tombol "Enter"
+        
         self.boxlogin.bind('<Return>', self.check)
 
     def check(self, event=None):
@@ -54,7 +53,6 @@ class Login:
         self.boxlogin.mainloop()
 
     def generate_captcha(self):
-        # Membuat CAPTCHA dengan angka acak
         num1 = random.randint(1, 10)
         num2 = random.randint(1, 10)
         operator = random.choice(['+', '-', '*'])
@@ -65,7 +63,7 @@ class Login:
         else:
             result = num1 * num2
         captcha_text = f'{num1} {operator} {num2} = ?'
-        return captcha_text, str(result)  # Mengembalikan teks CAPTCHA dan jawabannya
+        return captcha_text, str(result) 
 
 class Register:
     def __init__(self, master, users, callback):
@@ -74,7 +72,7 @@ class Register:
         self.jendela.geometry('300x350')
 
         self.users = users
-        self.callback = callback  # Tambahkan callback ke halaman login
+        self.callback = callback  
         self.username_register = tk.StringVar()
         self.password_register = tk.StringVar()
         self.cek_password = tk.StringVar()
@@ -92,24 +90,23 @@ class Register:
         self.confirm_password_entry = tk.Entry(self.jendela, textvariable=self.cek_password)
         self.confirm_password_entry.pack()
 
-        tk.Label(self.jendela, text='CAPTCHA: ' + self.captcha[0]).pack()  # Menampilkan CAPTCHA
+        tk.Label(self.jendela, text='CAPTCHA: ' + self.captcha[0]).pack()  
         self.captcha_entry = tk.Entry(self.jendela, textvariable=tk.StringVar())
-        self.captcha_entry.pack()  # Input untuk jawaban CAPTCHA
+        self.captcha_entry.pack()  
 
         tk.Button(self.jendela, text='Register', command=self.tombol_register).pack()
 
-        # Event binding untuk tombol "Enter"
         self.jendela.bind('<Return>', self.tombol_register)
 
-    def tombol_register(self, event=None):  # Menambahkan parameter event=None agar fungsi dapat dipanggil dari event binding atau secara manual
-        # Memeriksa apakah jawaban CAPTCHA benar
+    def tombol_register(self, event=None):
+        
         if self.captcha[1].lower() == self.captcha_entry.get().lower():
             if self.password_register.get() == self.confirm_password_entry.get():
                 if not self.ada_username(self.username_register.get()):
                     self.users[self.username_register.get()] = self.password_register.get()
                     messagebox.showinfo('Registrasi diterima', 'Selamat!')
-                    self.callback()  # Panggil kembali ke halaman login
-                    self.jendela.destroy()  # Hancurkan jendela registrasi setelah selesai
+                    self.callback() 
+                    self.jendela.destroy()
                 else:
                     messagebox.showerror('Register Gagal', 'Ulangi registrasi')
             else:
@@ -121,7 +118,6 @@ class Register:
         return username in self.users
 
     def generate_captcha(self):
-        # Membuat CAPTCHA dengan angka acak
         num1 = random.randint(1, 10)
         num2 = random.randint(1, 10)
         operator = random.choice(['+', '-', '*'])
@@ -132,7 +128,7 @@ class Register:
         else:
             result = num1 * num2
         captcha_text = f'{num1} {operator} {num2} = ?'
-        return (captcha_text, str(result))  # Mengembalikan teks CAPTCHA dan jawabannya
+        return (captcha_text, str(result))
 
 login = Login()
 login.run()
